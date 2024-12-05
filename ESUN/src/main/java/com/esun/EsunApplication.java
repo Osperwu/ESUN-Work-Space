e8851c60713be4250589a014ddfe0d77284782f4
Osperwu <97653683+Osperwu@users.noreply.github.com>
2024-12-05 17:38:30 +0800

    1.check db connection 2.add ignore file

--- ESUN/src/main/java/com/esun/EsunApplication.java
+++ ESUN/src/main/java/com/esun/EsunApplication.java
@@ -8,7 +8,26 @@ public class EsunApplication {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		CheckEnvironment();
+
 		SpringApplication.run(EsunApplication.class, args);
 	}
 
+		public static void CheckEnvironment() {
+			// 檢查 Java 運行時的基本資訊
+			String javaVersion = System.getProperty("java.version");
+			String javaVendor = System.getProperty("java.vendor");
+			String javaHome = System.getProperty("java.home");
+
+			System.out.println("Java Version: " + javaVersion);
+			System.out.println("Java Vendor: " + javaVendor);
+			System.out.println("Java Home: " + javaHome);
+
+			// 檢查是否是通過 JVM 運行
+			if (javaVersion != null && javaHome != null) {
+				System.out.println("This application is running in a Java environment.");
+			} else {
+				System.out.println("This application is not running in a Java environment.");
+			}
+		}
 }
--- ESUN/src/main/java/com/esun/controller/HomeController.java
+++ ESUN/src/main/java/com/esun/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.esun.controller;
 
+import com.esun.repository.UserRepository;
+import com.esun.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HomeController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/home")
     public String home() {
-        System.err.println("success");
+        System.err.println("success 成功進入系統");
+        List<User> allUser = userRepository.findAllUser();
+        System.out.println("全部使用者" + allUser);
         return "OK";
     }
 }
